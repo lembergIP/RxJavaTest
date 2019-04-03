f35fd592c28d008268735e4329a82cfff60a9223
ipasternak <ipasternak@ancestry.com>
2019-04-03 11:55:47 +0300

    first commit

---
+++ src/main/java/test/rxjava/ServiceRunner.java
@@ -0,0 +1,15 @@
+package test.rxjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRunner
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ServiceRunner.class, args);
+    }
+
+}
