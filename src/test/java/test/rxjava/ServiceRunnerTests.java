94d6aa21df9c630d82d53155230f10fb16e12e6f
ipasternak <ipasternak@ancestry.com>
2019-04-03 11:53:50 +0300

    first commit

---
+++ src/test/java/test/rxjava/ServiceRunnerTests.java
@@ -0,0 +1,18 @@
+package test.rxjava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceRunnerTests
+{
+
+    @Test
+    public void contextLoads()
+    {
+    }
+
+}
